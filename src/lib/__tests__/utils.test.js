858ddd574026918f4b48f92c580552e230fd5d1f
Rogelio Guzman <rogelioguzmanh@gmail.com>
2019-03-15 09:48:17 -0700

    Add some tests

--- src/lib/__tests__/utils.test.js
+++ src/lib/__tests__/utils.test.js
@@ -1,5 +1,5 @@
 import stripAnsi from 'strip-ansi';
-import { trimAndFormatPath } from '../utils';
+import { trimAndFormatPath, formatTestNameByPattern } from '../utils';
 
 test('trimAndFormatPath', () => {
   expect(
@@ -7,4 +7,35 @@ test('trimAndFormatPath', () => {
       trimAndFormatPath(2, { cwd: '/hello/there' }, '/hello/there/to/you', 80),
     ),
   ).toEqual('to/you');
+});
+
+describe('formatTestNameByPattern', () => {
+  test.each`
+    testName           | pattern   | width
+    ${'the test name'} | ${'the'}  | ${30}
+    ${'the test name'} | ${'the'}  | ${25}
+    ${'the test name'} | ${'the'}  | ${20}
+    ${'the test name'} | ${'the'}  | ${15}
+    ${'the test name'} | ${'the'}  | ${10}
+    ${'the test name'} | ${'the'}  | ${5}
+    ${'the test name'} | ${'test'} | ${30}
+    ${'the test name'} | ${'test'} | ${25}
+    ${'the test name'} | ${'test'} | ${20}
+    ${'the test name'} | ${'test'} | ${15}
+    ${'the test name'} | ${'test'} | ${10}
+    ${'the test name'} | ${'test'} | ${5}
+    ${'the test name'} | ${'name'} | ${30}
+    ${'the test name'} | ${'name'} | ${25}
+    ${'the test name'} | ${'name'} | ${20}
+    ${'the test name'} | ${'name'} | ${15}
+    ${'the test name'} | ${'name'} | ${10}
+    ${'the test name'} | ${'name'} | ${5}
+  `(
+    'formats when testname="$testName", pattern="$pattern", and width="$width"',
+    ({ testName, pattern, width }) => {
+      expect(
+        formatTestNameByPattern(testName, pattern, width),
+      ).toMatchSnapshot();
+    },
+  );
 });
